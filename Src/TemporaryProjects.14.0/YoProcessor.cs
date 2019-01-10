a36b2e9767ebacf36e6067e7292f7d858ef35759
Greg Trevellick <gtrevellick@googlemail.com>
2019-01-10 08:34:34 +0000

    .

--- Src/TemporaryProjects.14.0/YoProcessor.cs
+++ Src/TemporaryProjects.14.0/YoProcessor.cs
@@ -9,10 +9,8 @@ namespace CommonYo
 {
     public class YoProcessor
     {
-        private DirectoryInfo _solutionDirectoryInfo;
-        //private DirectorySystemDto _directorySystemDto;
-        //private FileSystemDto _fileSystemDto;
-        private string _generatorName;
+        //private DirectoryInfo _solutionDirectoryInfo;
+        //private string _generatorName;
 
         public YoProcessor()//string generatorName)
         {
@@ -44,62 +42,16 @@ namespace CommonYo
 
         public void Generate()
         {
-            GenerateYeomanProject();// (_directorySystemDto.SolutionDirectoryInfo.Parent.FullName);
+            CreateYoProjectOnDisc();
         }
 
-        private void GenerateYeomanProject()// string generationDirectory)
-        {
-            var assemblyLocation = Assembly.GetExecutingAssembly().Location;
-            var assemblyDirectory = Path.GetDirectoryName(assemblyLocation);
-
-            // Matt's code already caters for the ang-bas folder already existing - we don't need to check anything here
-            var yoBatchFile = $@"{assemblyDirectory}\yo.bat";
-            // Wrap each arg in quotes so that batch file caters for spaces in path names etc
-            //var args = $"\"{_generatorName}\" \"{generationDirectory}\" \"{assemblyDirectory}\"";
-            CreateYoProjectOnDisc(yoBatchFile);//, args);
-        }
-
-        //public void ArchiveRegularProject(string solutionDirectory, string tempDirectory)
-        //{
-        //    ArchiveRegularProject(solutionDirectory, tempDirectory, _solutionDirectoryInfo);
-        //}
-
-        //private void ArchiveRegularProject(string solutionDirectory, string tempDirectory, DirectoryInfo solutionDirectoryInfo)
-        //{
-        //    var archiveLocation = GetArchiveLocation(tempDirectory, solutionDirectoryInfo);
-        //    Directory.Move(solutionDirectory, archiveLocation);
-        //}
-
-        //private string GetArchiveLocation(string tempDirectory, DirectoryInfo solutionDirectoryInfo)
-        //{
-        //    var dateTime = GetUniqueDateTime();
-        //    var archiveLocation = $"{tempDirectory}\\{solutionDirectoryInfo.Name}_{dateTime}";
-        //    if (Directory.Exists(archiveLocation))
-        //    {
-        //        archiveLocation = GetUniqueLocation(archiveLocation);
-        //    }
-        //    return archiveLocation;
-        //}
-
-        //private string GetUniqueLocation(string location)
-        //{
-        //    var random = new Random();
-        //    return $"{location}_{random.Next(1, 999999)}";
-        //}
-
-        //private string GetUniqueDateTime()
-        //{
-        //    return $"{DateTime.UtcNow.Year}{DateTime.UtcNow.Month}{DateTime.UtcNow.Day}_{DateTime.UtcNow.Hour}{DateTime.UtcNow.Minute}{DateTime.UtcNow.Second}_{DateTime.UtcNow.Millisecond}";
-        //}
-
-        private void CreateYoProjectOnDisc(string batchFileToBeOpened)//, string args)
+        private void CreateYoProjectOnDisc()
         {
             var processStartInfo = new ProcessStartInfo()
             {
-                //Arguments = args,
                 CreateNoWindow = false,
-                FileName = batchFileToBeOpened,
-                UseShellExecute = false,
+                FileName = "Yo",
+                UseShellExecute = true,
                 WindowStyle = ProcessWindowStyle.Normal, 
             };
 
