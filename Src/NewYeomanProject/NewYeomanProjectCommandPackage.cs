ed0b3e32521277f7e140ad86c700e9f6a87e8bda
Greg Trevellick <gtrevellick@googlemail.com>
2019-01-12 20:38:28 +0000

    .

--- Src/NewYeomanProject/NewYeomanProjectCommandPackage.cs
+++ Src/NewYeomanProject/NewYeomanProjectCommandPackage.cs
@@ -8,7 +8,7 @@ using Task = System.Threading.Tasks.Task;
 namespace NewYeomanProject
 {
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [InstalledProductRegistration(productName: "#110", productDetails: "#112", productId: "1.0",        IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(PackageGuids.newYeomanProjectCommandPackageString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
